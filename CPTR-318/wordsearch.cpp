555db497f09261eb7154064e436c7cffab8f02e3
Miro Manestar <miroimanestar@gmail.com>
2020-08-28 14:49:05 -0400

    More changes to wordsearch

--- CPTR-318/wordsearch.cpp
+++ CPTR-318/wordsearch.cpp
@@ -4,29 +4,35 @@
 
 using namespace std;
 
+//What??? I like colored consoles....
+string red = "\u001b[31m";
+string green = "\u001b[32m";
+string yellow = "\u001b[33m";
+string blue = "\u001b[34m";
+string reset = "\u001b[0m";
+
+vector<vector<string>> puzzle;
+
+//Function definitions
+void makePuzzle();
+
 int main() {
 
-    //What??? I like colored consoles....
-    string red = "\u001b[31m";
-    string green = "\u001b[32m";
-    string yellow = "\u001b[33m";
-    string blue = "\u001b[34m";
-    string reset = "\u001b[0m";
-
-    makePuzzle(20, 20);
-
+    makePuzzle();
+    cout << "ddf";
     return 0;
 }
 
-string makePuzzle(int height, int width) {
+void makePuzzle() {
 
-    string puzzle[20][20]; 
+    for(int i = 0; i < puzzle.size(); i++) {
+        for(int j = 0; i < puzzle[i].size(); i++) {
 
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; i < width; i++) {
             puzzle[i][j] = ".";
+            cout << ".";
         }
+        cout << "\n";
     }
 
-    return puzzle;
+    return;
 }
