f2d6645f64e8009e0e3d6f5bb02b35977ed5b365
Miro Manestar <miroimanestar@gmail.com>
2020-09-20 02:16:49 -0400

    Ripping my hair out over how a triple-linked double linked list is supposed to work

--- Assignments/multilists/multilist.cpp
+++ Assignments/multilists/multilist.cpp
@@ -7,10 +7,49 @@
 
 #include "multilist.h"
 
-bool insert(int id, std::string name, int age) {
+Multilist::Multilist(void) {
+    int i = 0;
+}
+
+Multilist::Node::Node(int id, std::string name, int age): id(id), name(name), age(age) {
+    this->id = id;
+    this->name = name;
+    this->age = age;
+}
+
+bool Multilist::insert(int id, std::string name, int age) {
+    Node *node = new Node(id, name, age);
+    
+    first->next_id = node->prev_id;
+
+
+    return true;
+}
+
+bool Multilist::remove(int id) {
+    return true;
+}
+
+void Multilist::print_by_ID() {
 
 }
 
-bool remove(int id) {
+void Multilist::print_by_ID_reverse() {
+
+}
+
+void Multilist::print_by_name() {
+
+}
+
+void Multilist::print_by_name_reverse() {
+
+}
+
+void Multilist::print_by_age() {
+
+}
+
+void Multilist::print_by_age_reverse() {
 
 }
