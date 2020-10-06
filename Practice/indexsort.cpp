6a8e86c3b0557f7f81b7159241558e59714e3833
Miro Manestar <miroimanestar@gmail.com>
2020-10-06 14:47:07 -0400

    Got indexsort to work:

--- Practice/indexsort.cpp
+++ Practice/indexsort.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <algorithm>
 
 using namespace std;
+using namespace std::chrono;
 
 vector<int> index_sort(vector<int> v);
 void print_vec(vector<int> v);
 std::ostream& operator<<(std::ostream& os, const vector<int>& v);
 
 int main() {
-    vector<int> v{5, 30, 9, 8, 15, 22, 6};
+
+    //Build vector with v_size random numbers
+    int v_size = 10'000'000;
+    vector<int> v(v_size);
+    auto f = [v_size]() -> int { return rand() % v_size; };
+    generate(v.begin(), v.end(), f);
+
+    //Start timer for time measurement
+    auto start = high_resolution_clock::now();
+
+    //Sort vector v
     vector<int> sorted = index_sort(v);
-    print_vec(sorted);
+
+    //Stop timer and calculate duration
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    //print_vec(sorted);
+    cout << "\nProgram finished in " << duration.count()/1000 << "ms";
 }
 
 vector<int> index_sort(vector<int> v) {
+    //Find max value in v
     int max = 0;
     for (int val : v) {
         if(val > max) {
@@ -22,15 +42,19 @@ vector<int> index_sort(vector<int> v) {
         }
     }
 
-    vector<int> temp(max);
+    //Create new vector with a corresponding index for each number in the array
+    //Then set each corresponding index to "1" if it has an equivalent 
+    vector<int> temp(max + 1);
     for(int val : v) {
-        temp[val] = 1;
+        //Counts the number of times if there's a duplicate
+        temp[val] += 1;
     }
 
+    //Create new array composed if the indexes from temp that contained values greater than 0
     vector<int> sorted;
-    for(int val : temp) {
-        if(val != 0) {
-            sorted.push_back(val);
+    for(int i = 0; i < temp.size(); i++) {
+        if(temp[i] != 0) {
+            sorted.push_back(i);
         }
     }
 
