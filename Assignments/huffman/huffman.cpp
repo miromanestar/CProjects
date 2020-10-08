591fc76861a1e2f2115a8d667e777b5158229516
Miro Manestar <miroimanestar@gmail.com>
2020-10-08 12:09:18 -0400

    *Sigh

--- Assignments/huffman/huffman.cpp
+++ Assignments/huffman/huffman.cpp
@@ -13,9 +13,21 @@ using namespace std;
 
 void process_char(char character);
 void read_data();
+void sort_counts();
 void make_tree();
+void sort_table();
 
 vector<int> counts(26);
+vector<vector<int>> freqTable;
+
+//Small little class to store a linked-list for the huffman coding tree
+class Tree {
+    char letter;
+    struct Node {
+        Node *left, *right;
+    };
+    Node *first, *last;
+};
 
 int main() {
     read_data();
@@ -32,6 +44,33 @@ int main() {
     make_tree();
     cout << "\n--------------------------------\n";
 
+    make_table();
+
+    for(vector<int> val : freqTable) {
+        cout << val[0] << ": " << val[1] << '\n';
+    }
+}
+
+void make_table() {
+    for(int i = 0; i < counts.size(); i++) {
+        freqTable.push_back({i + 65, counts[i]});
+    }
+
+    sort_table();
+}
+
+void sort_table() {
+    for(int i = 0; i < freqTable.size(); i++) {
+        vector<int> min = freqTable[0];
+        for(int k = i + 1; k < freqTable.size(); k++) {
+            if(freqTable[k][1] < min[1]) {
+                min = freqTable[k];
+            }
+        }
+        vector<int> temp = freqTable[i];
+        freqTable[i] = min;
+        
+    }
 }
 
 void make_tree() {
