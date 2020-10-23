c22520fbe6e9d1aae289f02596791d56ed096413
Miro Manestar <miroimanestar@gmail.com>
2020-10-23 11:01:03 -0400

    Added maze placeholder

---
+++ Assignments/maze/maze.cpp
@@ -0,0 +1,14 @@
+//  Name: Miro Manestar
+//  Assignment number: 4
+//  Assignment: Maxe Generation
+//  File name: maze.cpp
+//  Date last modified: October 23, 2020
+//  Honor statement: I have neither given nor received any unauthorized help on this assignment.
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    
+}
