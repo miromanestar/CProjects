6e06495536f9cc468ef5213151998b47b9ebc59d
Miro Manestar <miroimanestar@gmail.com>
2020-10-29 22:53:46 -0400

    Worked on maze

--- Assignments/maze/manestar.cpp
+++ Assignments/maze/manestar.cpp
@@ -9,64 +9,105 @@
 #include <vector>
 #include <GL/sgl.hpp>
 
-class MazeWindow : public sgl::Window {
+class Maze : public sgl::Window {
+    int cells = 0;
+    int cellsX;
+    int cellsY;
+    double cellWidth;
+    double cellHeight;
 public:
-    MazeWindow(double width, double height):
-        sgl::Window("Maze Generator - Miro Manestar", 0, width, 0, height) { }
-
-        void paint() override {
-            sgl::set_color(sgl::BLACK);
-            draw_cells(25, 40);
+    Maze(double width, double height, int cellsX, int cellsY): cellsX(cellsX), cellsY(cellsY),
+        sgl::Window("Maze Generator - Miro Manestar", 0, width, 0, height) { 
+            cellWidth = get_max_x()/(cellsX + 2);
+            cellHeight = get_max_y()/(cellsY + 2);
         }
 
-        void draw_cells(int cellsX, int cellsY) {
-            int cells = 0;
+    void paint() override {
+        sgl::set_color(sgl::BLACK);
+        draw_cells();
+        make_maze();
+    }
 
-            double cellWidth = get_max_x()/(cellsX + 2);
-            double cellHeight = get_max_y()/(cellsY + 2);
-            
-            //Build the initial maze
-            for (int y = 1; y <= cellsY; y++) {
-                for (int x = 1; x <= cellsX; x++) {
-                    sgl::draw_rectangle(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
-                    sgl::draw_text(std::to_string(cells), x * cellWidth + cellWidth/2, y * cellHeight + cellHeight/2, 12);
+    void draw_cells() {
+        //Build the initial maze
+        for (int y = 1; y <= cellsY; y++) {
+            for (int x = 1; x <= cellsX; x++) {
+                sgl::draw_rectangle(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+                sgl::draw_text(std::to_string(cells), x * cellWidth + cellWidth/2, y * cellHeight + cellHeight/2, 12);
 
-                    //Remove wall for entrance and exit
-                    if (cells == 0) {
-                        sgl::set_color(sgl::WHITE);
-                        sgl::draw_line(x * cellWidth, y * cellHeight, x * cellWidth, y * cellHeight + cellHeight);
-                        sgl::set_color(sgl::BLACK);
-                    } else if (x == cellsX && y == cellsY) {
-                        sgl::set_color(sgl::WHITE);
-                        sgl::draw_line(x * cellWidth + cellWidth, y * cellHeight, x * cellWidth + cellWidth, y * cellHeight + cellHeight);
-                        sgl::set_color(sgl::BLACK);
-                    }
-
-                    cells++;
+                //Remove wall for entrance and exit
+                if (cells == 0) {
+                    sgl::set_color(sgl::WHITE);
+                    sgl::draw_line(x * cellWidth, y * cellHeight, x * cellWidth, y * cellHeight + cellHeight);
+                    sgl::set_color(sgl::BLACK);
+                } else if (x == cellsX && y == cellsY) {
+                    sgl::set_color(sgl::WHITE);
+                    sgl::draw_line(x * cellWidth + cellWidth, y * cellHeight, x * cellWidth + cellWidth, y * cellHeight + cellHeight);
+                    sgl::set_color(sgl::BLACK);
                 }
+
+                cells++;
             }
         }
+    }
+
+    void make_maze() {
+        //Retool Maze to utilize a 2d array and draw a maze from that
+    }
 };
 
 class DisjointSet {
+    std::vector<int> set;
 public:
-    int find(int i) {
-
+    DisjointSet(int numElements): set(numElements) { 
+        for (int i = 0; i < (int) set.size(); i++) {
+            set[i] = i;
+        }
     }
 
-    int combine(int a, int b) {
-
+    //Returns the "parent" element of an equivalence class
+    int Find(int i) {
+        if (set[i] != i)
+            set[i] = Find(set[i]);
+        return set[i];
     }
 
-    int split(int a, int b) {
-
+    //Combines two equivalence classes, smaller class is pointed to the larger class
+    void Union(int a, int b) {
+        int set1 = Find(a);
+        int set2 = Find(b);
+        if (Cardinality(set1) >= Cardinality(set2))
+            set[set1] = set2;
+        else
+            set[set2] = set1;
     }
 
-    int cardinality(int a, int b) {
-        
+    //Takes a set and puts each element from it inside its own equivalence class
+    void Split(int a) {
+        std::vector<int> temp;
+        for (int i = 0; i < (int) set.size(); i++) {
+            if (Find(set[i]) == a) {
+                temp.push_back(i);
+            }
+        }
+
+        for (int item : temp) {
+            set[item] = item;
+        }
+    }
+
+    //Returns the number of items in a set
+    int Cardinality(int a) {
+        int count = 0;
+        for (int i = 0; i < (int) set.size(); i++) {
+            if (Find(set[i]) == a)
+                count++;
+        }
+
+        return count;
     }
 };
 
 int main() {
-    sgl::run<MazeWindow>(600.0, 400.0);
+    sgl::run<Maze>(600.0, 400.0, 25, 40);
 }
