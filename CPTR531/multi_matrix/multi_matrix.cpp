abad7ccc9b344998af9af3408a7acc55b8082541
Miro Manestar <miroimanestar@gmail.com>
2022-10-17 14:11:06 -0400

    Added (broken) multithreaded support

--- CPTR531/multi_matrix/multi_matrix.cpp
+++ CPTR531/multi_matrix/multi_matrix.cpp
@@ -1,6 +1,8 @@
+#include <thread>
 #include <vector>
 #include <cstdlib>
 #include <iostream>
+#include "stopwatch.h"
 
 using namespace std;
 using Matrix = vector<vector<int>>;
@@ -12,26 +14,60 @@ Matrix singleMatrix(Matrix &a, Matrix &b);
 void printMatrix(const Matrix &a);
 
 int main() {
-    auto a = genMatrix(3, 3);
-    auto b = genMatrix(3, 3);
+    int r = 1000;
+    int c = 1000;
+    auto a = genMatrix(r, c);
+    auto b = genMatrix(r, c);
 
-    printMatrix(a);
-    cout << "-----------------" << endl;
-    printMatrix(b);
-    cout << "-----------------" << endl;
-    printMatrix(singleMatrix(a, b));
+    Stopwatch timer;
+
+    // printMatrix(a);
+    // cout << "-----------------" << endl;
+    // printMatrix(b);
+    // cout << "-----------------" << endl;
+    cout << "Beginning single thread multiplication" << endl;
+    timer.start();
+    // printMatrix(singleMatrix(a, b));
+    singleMatrix(a, b);
+    timer.stop();
+    double single_time = timer.elapsed();
+
+    timer.reset();
+    
+    //cout << "-----------------" << endl;
+    cout << "Beginning multi thread multiplication" << endl;
+    timer.start();
+    // printMatrix(multiMatrix(a, b));
+    multiMatrix(a, b);
+    timer.stop();
+    double multi_time = timer.elapsed();
+
+    cout << "\n\n-----------------" << endl;
+    cout << "Single thread time: " << single_time << " ms." << endl;
+    cout << "Multi thread time: " << multi_time << " ms." << endl;
 }
 
-Matrix genMatrix (int r, int c) {
-    Matrix matrix;
+Matrix multiMatrix(Matrix &a, Matrix &b) {
+    int r = a.size();
+    int c = b[0].size();
+    Matrix result(r, vector<int>(c, 0));
+
+    vector<thread> threads;
     for (int i = 0; i < r; i++) {
-        vector<int> row;
         for (int j = 0; j < c; j++) {
-            row.push_back(rand() % 10);
+            threads.push_back(thread([&a, &b, &result, i, j] {
+                for (int k = 0; k < (int) a[0].size(); k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }));
         }
-        matrix.push_back(row);
     }
-    return matrix;
+
+    for (auto &t : threads) {
+        t.join();
+    }
+
+    return result;
 }
 
 Matrix singleMatrix(Matrix &a, Matrix &b) {
@@ -49,6 +85,18 @@ Matrix singleMatrix(Matrix &a, Matrix &b) {
         matrix.push_back(row);
     }
 
+    return matrix;
+}
+
+Matrix genMatrix (int r, int c) {
+    Matrix matrix;
+    for (int i = 0; i < r; i++) {
+        vector<int> row;
+        for (int j = 0; j < c; j++) {
+            row.push_back(rand() % 10);
+        }
+        matrix.push_back(row);
+    }
     return matrix;
 }
 
