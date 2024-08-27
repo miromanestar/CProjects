05fa66cd7242939ac77464c2f905d92a9906c210
Miro Manestar <miroimanestar@gmail.com>
2024-08-27 17:52:45 -0400

    Tadd lol

---
+++ Tadd/read_data/readfile.cpp
@@ -0,0 +1,30 @@
+#include "readfile.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> read_file(const std::string& filename) {
+    std::vector<std::string> words;
+
+    std::ifstream file(filename);
+    std::string str;
+    while (std::getline(file, str, ' ')) {
+        words.push_back(str);
+    }
+
+    return words;
+}
+
+int main(int argc, char *argv[]) {
+    if (argv[1] == nullptr) {
+        std::cout << "No file given";
+        return 0;
+    }
+
+    std::string filename(argv[1]);
+    auto words  = read_file(filename);
+
+    for (int i = 0; i < (int) words.size(); i++) {
+        std::cout << words[i] << "\n";
+    }
+}
---
+++ Tadd/read_data/readfile.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> read_file(const std::string& filename);
